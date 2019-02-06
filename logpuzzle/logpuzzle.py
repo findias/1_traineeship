98d0e8253bc704c06a249454b8c4d08d97319dcc
findias <findias@bk.ru>
2019-02-06 22:15:57 +0300

    Выполнена вторая часть задания

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -17,8 +17,6 @@ Given an apache logfile, find the puzzle urls and download the images.
 Here's what a puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 """
-
-
 def read_urls(filename):
   """Returns a list of the puzzle urls from the given log file,
   extracting the hostname from the filename itself.
@@ -59,9 +57,40 @@ def download_images(img_urls, dest_dir):
   Creates the directory if necessary.
   """
   # +++your code here+++
-  some_path = os.mkdir(dest_dir + )
+
+  # This code can create path right, but i don,t  understand ho it work
+
+  # try:
+  #   os.mkdir(dest_dir)
+  # except OSError as e:
+  #   if e.errno != errno.EEXIST:
+  #       raise
+
+  # Create path
+  if not os.path.exists(dest_dir):
+    os.mkdir(dest_dir)
+  x = os.path.abspath(dest_dir)
+  # Downloads file
+  n = 0
+  list_files =[]
   for url_item in img_urls:
-    urllib.urlretrieve(url_item,)
+    urllib.urlretrieve(url_item, str(x) +'\img' + str(n))
+    list_files.append(str(x) +'\img' + str(n))
+    n += 1
+    print "Retrieving..."
+  # Create html file
+
+  head = '<verbatim>\n<html>\n<body>\n'
+  footer = '</body>\n</html>'
+  with open(str(x)+'/index.html', 'w') as f:
+    f.write(head)
+    for file in list_files:
+      f.write('<img src="' + file + '">')
+    f.write(footer)
+
+
+
+
 
 def main():
   args = sys.argv[1:]
